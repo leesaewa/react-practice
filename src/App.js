fcef68d4db4651ac4590e1771024390006d5cd18
leesaewa <saewa0424@gmail.com>
2022-11-11 23:23:15 +0900

    221111:coin api

--- src/App.js
+++ src/App.js
@@ -1,49 +1,31 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState(""); //setToDo는 toDo값을 수정하는 함수.
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return alert("Please your to do list.");
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo(""); // input값 비우기
-  };
-
-  // delete
-
-  const onDelete = (index) => {
-    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
-  };
-
-  console.log(toDos);
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []); // 빈 배열-> 한 번만 동작
   return (
-    <div className={styles.wrapper}>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => onDelete(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              {coin.name} ({coin.symbol}):{coin.quotes.USD.price} USD
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
-
 export default App;
