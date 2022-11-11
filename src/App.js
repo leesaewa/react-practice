c0cfc0ce9c3f408a577bbc13e89f1c218c4857d0
leesaewa <saewa0424@gmail.com>
2022-11-12 02:27:28 +0900

    221112:end

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
-import { useState, useEffect } from "react";
+// router를 render시키는 App.js파일
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-
-    // 위는 아래의 간편 버전
-    // fetch(
-    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    // )
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     setMovies(json.data.movies);
-    //     setLoading(false);
-    //   });
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-
   return (
-    <div>
-      {loading ? (
-        <h1>loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <h2>{movie.title}</h2>
-              <img src={movie.medium_cover_image} />
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((genre) => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/routes/Home.js
@@ -0,0 +1,58 @@
+// 기본적으로 로딩하거나 영화 리스트를 보여주는 화면
+
+import { useState, useEffect } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+
+    // 위는 아래의 간편 버전
+    // fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    // )
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     setMovies(json.data.movies);
+    //     setLoading(false);
+    //   });
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
